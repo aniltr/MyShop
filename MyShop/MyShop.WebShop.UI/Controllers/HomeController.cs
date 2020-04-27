685759872e994bdf4791b6b6f5c58e00866ca738
anil <aniltr@gmail.com>
2020-04-27 19:01:09 +1000

    Implemented Product Listing

--- MyShop/MyShop.WebShop.UI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebShop.UI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,31 @@ namespace MyShop.WebShop.UI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> dataContext;
+        IRepository<ProductCategory> categoryContext;
+        public HomeController(IRepository<Product> dataContext, IRepository<ProductCategory> categoryContext)
+        {
+            this.dataContext = dataContext;
+            this.categoryContext = categoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = this.dataContext.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            Product product = dataContext.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
